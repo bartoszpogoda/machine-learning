f864adb407272f563539227527082b78e982066a
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-01-28 19:07:13 +0100

    scorer for classifier impl

---
+++ src/components/ClassifierScorer.java
@@ -0,0 +1,19 @@
+package components;
+
+import algorythm.Classifier;
+import exceptions.DataNotCompatibleException;
+import exceptions.DataNotLearnedException;
+import exceptions.DataValidatorNotSetException;
+import exceptions.DependenciesNotSetException;
+import weka.core.Instances;
+
+public interface ClassifierScorer {
+	void setClassifier(Classifier classifier);
+	void setLearnData(Instances learnData);
+	void setTestData(Instances testData);
+	void setCrossValidationData(Instances crossValidationData);
+	void setCrossValidationFolds(int crossValidationFolds);
+	
+	// returns accuracy
+	double score(boolean crossValidation) throws DependenciesNotSetException, DataValidatorNotSetException, DataNotLearnedException, DataNotCompatibleException;
+}
---
+++ src/components/impl/ClassifierScorerImpl.java
@@ -0,0 +1,95 @@
+package components.impl;
+
+import java.util.Enumeration;
+
+import algorythm.Classifier;
+import components.ClassifierScorer;
+import exceptions.DataNotCompatibleException;
+import exceptions.DataNotLearnedException;
+import exceptions.DataValidatorNotSetException;
+import exceptions.DependenciesNotSetException;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class ClassifierScorerImpl implements ClassifierScorer {
+
+	private Classifier classifier;
+	
+	private Instances learnData, testData, crossValidationData;
+
+	private int crossValidationFolds = 0;
+	
+	@Override
+	public void setClassifier(Classifier classifier) {
+		this.classifier = classifier;
+	}
+
+	@Override
+	public void setLearnData(Instances learnData) {
+		this.learnData = learnData;
+	}
+
+	@Override
+	public void setTestData(Instances testData) {
+		this.testData = testData;
+	}
+	
+	public void setCrossValidationData(Instances crossValidationData){
+		this.crossValidationData = crossValidationData;
+	}
+	
+	public void setCrossValidationFolds(int crossValidationFolds){
+		this.crossValidationFolds = crossValidationFolds;
+	}
+
+	@Override
+	public double score(boolean crossValidation) throws DependenciesNotSetException, DataValidatorNotSetException, DataNotLearnedException, DataNotCompatibleException {
+		
+		int allPredictions = 0;
+		int correctPredictions = 0;
+		
+		if(crossValidation){
+			
+			if(classifier == null || crossValidationData == null || crossValidationFolds == 0) throw new DependenciesNotSetException();
+			
+			for(int i = 0 ; i < crossValidationFolds ; i++){
+				Instances cvLearnData = crossValidationData.trainCV(crossValidationFolds, i);
+				Instances cvTestData = crossValidationData.testCV(crossValidationFolds, i);
+				
+				classifier.learn(cvLearnData);
+				Instances predictedData = classifier.predict(cvTestData);
+				
+				// accuracy tests
+				
+				Enumeration<Instance> enumerateReal = cvTestData.enumerateInstances();
+				Enumeration<Instance> enumeratePredicted = predictedData.enumerateInstances();
+				
+				while(enumerateReal.hasMoreElements() && enumeratePredicted.hasMoreElements()){
+					Instance real = enumerateReal.nextElement();
+					Instance predicted = enumeratePredicted.nextElement();
+					
+					allPredictions++;
+					
+					String realClass = real.stringValue(real.dataset().attribute("class"));	
+					String predictedClass = predicted.stringValue(predicted.dataset().attribute("class"));
+					
+					if(predictedClass.equalsIgnoreCase(realClass))
+							correctPredictions++;
+					
+				}
+				
+				
+			}
+			
+			
+		} else{
+			
+			if(classifier == null || learnData == null || testData == null) throw new DependenciesNotSetException();
+			
+			
+		}
+		
+		return ((double)correctPredictions/(double)allPredictions);
+	}
+
+}
